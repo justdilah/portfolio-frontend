2714a7a73e1cda2684b5e852ddd50cafda354acf
justdilah <nurdilah352@gmail.com>
2023-05-25 12:14:34 +0800

    Setup for heroku hosting done.

---
+++ experiences.js
@@ -0,0 +1,23 @@
+const experiences = [
+    {
+        CompanyName: "ST Electonics",
+        JobPosition: "Software Engineer Intern",
+        Description: "• Created test scripts for the web application using JMeter to determine the application’s performance under real-life load conditions before deployment and generated the results in the dashboard report\n\n" +
+            "• Performed Black Box Testing on web application as part of the testing and integration stage",
+        Period: "Mar 2019 - Aug 2019",
+        Skills: ["JMeter","Java"]
+    },
+    {
+        CompanyName: "CPF Board",
+        JobPosition: "RPA/IPA Intern",
+        Description: "• Adopted an agile methodology with a pair-programming technique to develop a booking system app for a CPFB use case using Power App and Power Automate\n\n" +
+            "• Implemented UiPath Script for COVID-19 Tracking CPFB use case to automate internal user’s mundane task\n\n" +
+            "• Developed VBA script for CPFB use case to send out bulk emails to multiple recipients\n\n" +
+            "• Assisted in Basic In-House UiPath Training, clarifying doubts and technical issues experienced by trainees\n\n" +
+            "• Implemented Python Script for Data Migration, significantly reducing the time taken and need for manual data entry for CPFB Service Center Officers",
+        Period: "May 2022 - Dec 2022",
+        Skills: ["UiPath","VBA","Python","Microsoft Power Apps","Microsoft Power Apps"]
+    }
+];
+
+module.exports = {experiences};
